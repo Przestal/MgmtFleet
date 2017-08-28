f30dbd217e3c90ff84821ed602703e55e36cca20
Przemysław <Megatrenq3>
2017-08-28 22:25:48 +0200

    added list

--- src/main/java/org/project/App.java
+++ src/main/java/org/project/App.java
@@ -1,44 +1,31 @@
 package org.project;
 
 
-import com.sun.org.apache.bcel.internal.generic.SWITCH;
+import org.project.mgmtCars.CarsFactory;
 
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.lang.IndexOutOfBoundsException;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
 
 
     public static void main(String[] args )
 
     {
-
-        List<String> list = new ArrayList<String>();
-        int Kr123Po = 0;
-        list.add( Kr123Po,"Volvo Cx90");
-        list.add("Renault");
-        list.add("Mercedes");
-
-        String [] type = new String[8];
-        type[0] = "Volvo";
-        list.size();
-        list.set(Kr123Po,"Vos");
-        System.out.println(list);
-
-        System.out.println(type[0]);
-
-        System.out.println(list);
+        List<CarsType> carsTypeList = CarsFactory.getCarsType();
 
 
 
+        for (CarsType carsType : carsTypeList) {
+
+
+
+            System.out.println("Car brand: "+carsType.getCarBrand());
+            System.out.println("Car mileage: "+carsType.getCarMileage().toString());
+            System.out.println("Car weight: "+carsType.getWeightCar().toString());
+            System.out.println("-------------------");
+
+        }
     }
 
 }
--- src/main/java/org/project/mgmtCars/CarsFactory.java
+++ src/main/java/org/project/mgmtCars/CarsFactory.java
@@ -2,6 +2,8 @@ package org.project.mgmtCars;
 
 import org.project.CarsType;
 import org.project.brandCars.Iveco;
+import org.project.brandCars.Renault;
+import org.project.brandCars.Solaris;
 import org.project.typeCars.Car;
 
 import java.util.ArrayList;
@@ -9,7 +11,18 @@ import java.util.List;
 
 public class CarsFactory {
 
+public static List<CarsType> getCarsType(){
+List<CarsType> carsTypes = new ArrayList<>();
 
+carsTypes.add(new Iveco("STRALIS",9820.0, 326855));
+carsTypes.add(new Renault("SCENIC",1428.2,186000));
+carsTypes.add(new Solaris("URBINO 12",13200.0,500000));
+
+return carsTypes;
+
+
+
+}
 
 
 
